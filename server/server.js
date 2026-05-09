5ce01b44f9240a2521f3cbf4f2dea7908ed69cbf
Muhammed Aashiq <aashiqmuhammed007@gmail.com>
2026-05-10 00:24:24 +0530

    jwt

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes.js");
+
+
+
 const connectDB = require("./config/db");
 
 const app = express();
@@ -10,6 +14,8 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req,res) => {
     res.send("Habit tracker api running");
